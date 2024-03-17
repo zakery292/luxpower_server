365f5e4477eaee370e61d09aa206d61973c619e4
zakery292 <112213249+zakery292@users.noreply.github.com>
2024-03-17 00:10:44 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/with-contenv bashio
-
+echo "Starting Node Js Server"
 # Load options from Home Assistant add-on configuration
+echo "Lodoing Congifg"
 DONGLE_IP=$(bashio::config 'dongleIP')
-HOME_ASSISTANT_IP=$(bashio::config 'homeAssistantIP')
-SEND_TO_LUX=$(bashio::config 'sendToLUX')
-SEND_TO_HOME_ASSISTANT=$(bashio::config 'sendToHomeAssistant')
 echo "DONGLE_IP: $DONGLE_IP"
+HOME_ASSISTANT_IP=$(bashio::config 'homeAssistantIP')
 echo "HOME_ASSISTANT_IP: $HOME_ASSISTANT_IP"
+SEND_TO_LUX=$(bashio::config 'sendToLUX')
 echo "SEND_TO_LUX: $SEND_TO_LUX"
+SEND_TO_HOME_ASSISTANT=$(bashio::config 'sendToHomeAssistant')
 echo "SEND_TO_HOME_ASSISTANT: $SEND_TO_HOME_ASSISTANT"
 
 export DONGLE_IP
@@ -20,5 +21,7 @@ export DONGLE_IP HOME_ASSISTANT_IP SEND_TO_LUX SEND_TO_HOME_ASSISTANT
 # Output the IP address of the Node.js server
 echo "Node.js server will be accessible on the following IP address(es): $(hostname -I)"
 
+
 # Start the Node.js application
 node /usr/src/app/app.js
+echo "Node Js server has Started"
