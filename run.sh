7fcdd1fbd64e053b961679792a6a77684dab2e94
zakery292 <112213249+zakery292@users.noreply.github.com>
2024-03-17 00:01:02 +0000

    Update run.sh
    Updates to force settings

--- run.sh
+++ run.sh
@@ -5,7 +5,15 @@ DONGLE_IP=$(bashio::config 'dongleIP')
 HOME_ASSISTANT_IP=$(bashio::config 'homeAssistantIP')
 SEND_TO_LUX=$(bashio::config 'sendToLUX')
 SEND_TO_HOME_ASSISTANT=$(bashio::config 'sendToHomeAssistant')
+echo "DONGLE_IP: $DONGLE_IP"
+echo "HOME_ASSISTANT_IP: $HOME_ASSISTANT_IP"
+echo "SEND_TO_LUX: $SEND_TO_LUX"
+echo "SEND_TO_HOME_ASSISTANT: $SEND_TO_HOME_ASSISTANT"
 
+export DONGLE_IP
+export HOME_ASSISTANT_IP
+export SEND_TO_LUX
+export SEND_TO_HOME_ASSISTANT
 # Export the variables so they are accessible to the Node.js app
 export DONGLE_IP HOME_ASSISTANT_IP SEND_TO_LUX SEND_TO_HOME_ASSISTANT
 
