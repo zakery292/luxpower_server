a38310aacc7a53ba0d9ca6de61a3f9e25c9401f1
zakery292 <112213249+zakery292@users.noreply.github.com>
2024-03-17 01:47:34 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 echo "Starting Node Js Server"
-# Load options from Home Assistant add-on configuration
-echo "Lodoing Congifg"
-DONGLE_IP=$(bashio::config 'dongleIP')
+# Load options from environment variables
+echo "Loading Config"
+
+DONGLE_IP=${DONGLE_IP:-null}
 echo "DONGLE_IP: $DONGLE_IP"
-HOME_ASSISTANT_IP=$(bashio::config 'homeAssistantIP')
+
+HOME_ASSISTANT_IP=${HOME_ASSISTANT_IP:-null}
 echo "HOME_ASSISTANT_IP: $HOME_ASSISTANT_IP"
-SEND_TO_LUX=$(bashio::config 'sendToLUX')
+
+# Assuming these are boolean values, defaulting to false if not set
+SEND_TO_LUX=${SEND_TO_LUX:-false}
 echo "SEND_TO_LUX: $SEND_TO_LUX"
-SEND_TO_HOME_ASSISTANT=$(bashio::config 'sendToHomeAssistant')
+
+SEND_TO_HOME_ASSISTANT=${SEND_TO_HOME_ASSISTANT:-false}
 echo "SEND_TO_HOME_ASSISTANT: $SEND_TO_HOME_ASSISTANT"
 
-export DONGLE_IP
-export HOME_ASSISTANT_IP
-export SEND_TO_LUX
-export SEND_TO_HOME_ASSISTANT
-# Export the variables so they are accessible to the Node.js app
 export DONGLE_IP HOME_ASSISTANT_IP SEND_TO_LUX SEND_TO_HOME_ASSISTANT
 
 # Output the IP address of the Node.js server
 echo "Node.js server will be accessible on the following IP address(es): $(hostname -I)"
 
-
 # Start the Node.js application
 node /usr/src/app/app.js
-echo "Node Js server has Started"
+echo "Node.js server has Started"
